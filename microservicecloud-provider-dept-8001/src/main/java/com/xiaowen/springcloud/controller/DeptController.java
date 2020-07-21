e1b5ade7f35731a1561d615321b172616ef8e708
wen.he <wen.he@supwisdom.com>
2020-07-21 14:38:55 +0800

    eureka集成，注册、发现

--- microservicecloud-provider-dept-8001/src/main/java/com/xiaowen/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/xiaowen/springcloud/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.xiaowen.springcloud.controller;
 import com.xiaowen.springcloud.entities.Dept;
 import com.xiaowen.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,6 +18,9 @@ public class DeptController {
 
   @Autowired
   private DeptService deptService;
+
+  @Autowired
+  private DiscoveryClient discoveryClient;
 
   @RequestMapping(value="/dept/add",method= RequestMethod.POST)
   public boolean add(@RequestBody Dept dept)
@@ -35,5 +40,25 @@ public class DeptController {
     return deptService.list();
   }
 
+  /**
+   * 服务发现
+   * @return
+   */
+  @RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+  public Object discovery()
+  {
+    //获取所有注册的微服务
+    List<String> list = discoveryClient.getServices();
+    System.out.println("**********" + list);
+
+    //获取指定的微服务实列
+    List<ServiceInstance> srvList = discoveryClient.getInstances("MICROSERVICECLOUD-DEPT");
+    //详细信息
+    for (ServiceInstance element : srvList) {
+      System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+          + element.getUri());
+    }
+    return this.discoveryClient;
+  }
 
 }
